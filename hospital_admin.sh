aa92ecb8159cf8b452061c3a941d4cfd03e1e310
Umutoni-Rita <umurita37@gmail.com>
2026-09-22 15:21:25 +0200

    Initialize System Script

---
+++ hospital_admin.sh
@@ -0,0 +1,16 @@
+initialize_system() {
+    if [ ! -d "active_logs" ]; then
+        echo "Creating active_logs directory..."
+        mkdir active_logs
+    fi
+
+    if [ ! -d "archived_logs" ]; then
+        echo "Creating archived_logs directory..."
+        mkdir archived_logs
+    fi
+
+    if [ ! -d "reports" ]; then
+        echo "Creating reports directory..."
+        mkdir reports
+    fi
+}
